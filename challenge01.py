8c49795cfb7b1b331b3b0ddede0a508c0cee5a74
oAmorim2k5 <victorhuski@gmail.com>
2024-07-08 13:50:54 -0300

    Calculadora conectada com JSON - W & R, começando banco de dados

--- challenge01.py
+++ challenge01.py
@@ -1,16 +1,16 @@
 import os, json
 
-def salvar_dados_json(dados, nome_arquivo): #Salvando operações em um arquivo JSON
+def salvar_operacoes_json(operacoes, nome_arquivo): #Salvando operações em um arquivo JSON
     try:
         with open(nome_arquivo, 'w') as arquivo_json:
-            json.dump(dados, arquivo_json, indent=4)
+            json.dump(operacoes, arquivo_json, indent=4)
     except Exception as e:
         print(f"Erro ao salvar os dados: {e}")
 
-def ler_dados_json(nome_arquivo): #Lendo operações em um arquivo JSON
+def ler_operacoes_json(nome_arquivo): #Lendo operações em um arquivo JSON
     try:
         with open(nome_arquivo, "r") as arquivo_json:
-            dados = json.load(arquivo_json)
+            operacoes = json.load(arquivo_json)
             print(f"Dados de operações:\n ${nome_arquivo}")
     except Exception as e:
         print(f"Erro ao ler os dados: ${e}")
@@ -20,7 +20,6 @@ def continua(result): #Printa resultado e continua após ação
     input("Pressione Enter para continuar...")
 
 def calc(numUm, operador, numDois): #calculadora
-    result = 0
     try:
         if operador == "+": #Somar
             return numUm + numDois
@@ -33,25 +32,30 @@ def calc(numUm, operador, numDois): #calculadora
                 return numUm / numDois
             else:
                 return "Operador invalido, divisão por 0"
+        elif operador == "%": #Resto da divisão
+            if numDois != 0:
+                return numUm % numDois
+            else:
+                return "Operador invalido, resto divisão por 0"
         elif operador == "**": #Exponenciação
             return numUm ** numDois
-        dados = {
-            "primeiroNumero": numUm,
-            "segundoNumero": numDois,
-            "operador": operador,
-            "resultado": result
-        } 
-        return dados
-        
+        elif operador == "=":
+            if numUm == numDois:
+                return True
+            else:
+                return False
+
     except Exception as e:
         return f"Erro: {str(e)}"
     
-if __name__ == "__main__": 
+if __name__ == "__main__":
+    nome_arquivo = "challenges01.json" #Arquivo json
+    operacoes = []
+    id = 0
     while True: #MAIN
         os.system("cls")
         try:
-            nome_arquivo = "challenges01.json" #Arquivo json
-
+            id += 1
             print("Exemplo de entrada: num01 + num02")
             operacao = input("Operação: ").split(" ")
 
@@ -59,16 +63,28 @@ if __name__ == "__main__":
             operador = str(operacao[1])
             numDois = float(operacao[2])
 
-            nova_operacao = calc(numUm, operador, numDois)
-        
-            continua(nova_operacao)
+            result = calc(numUm, operador, numDois)
+
+            dados = {
+            "id": id,
+            "primeiroNumero": numUm,
+            "segundoNumero": numDois,
+            "operador": operador,
+            "resultado": result
+            } 
+            operacoes.append(dados)
+            salvar_operacoes_json(operacoes, nome_arquivo)
+            continua(result)
 
         except ValueError:
+            id -= 1
             print("Erro: Entrada inválida. Certifique-se de digitar números e operadores corretamente.")
             input("Pressione Enter para continuar...")
         except IndexError:
+            id -= 1
             print("Erro: Formato de operação inválido. Use o formato 'numero operador numero'.")
             input("Pressione Enter para continuar...")
         except Exception as e:
+            id -= 1
             print(f"Erro inesperado: {str(e)}")
-            input("Pressione Enter para continuar...")            
+            input("Pressione Enter para continuar...")
